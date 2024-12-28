df3f7ac502efada3d8939fc8d9e813ccb07fda1d
panduzetiaa <pandupandusetiaanugrah0281@gmail.com>
2024-12-28 08:00:14 +0700

    perbaikan

--- modul 8/Assignment.java
+++ modul 8/Assignment.java
@@ -1,8 +1,8 @@
 public class Assignment {
     private double mark=-1;
-    Assignment () {
+    Assignment () {}
         public double getMark() throws NotYetSetException {
-            if (mark=-1) {
+            if (mark==-1) {
                 throw new NotYetSetException("Mark is not yet set");
             }
             return mark;
--- modul 8/CourseRecord.java
+++ modul 8/CourseRecord.java
@@ -1,2 +1,9 @@
+import java.util.Vector;
+
 public class CourseRecord {
+    private Student student;
+    private Vector assignments;
+    CourseRecord() {
+        assignment=new Vector();
+    }
 }
